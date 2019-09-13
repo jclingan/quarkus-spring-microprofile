7bc7f5089bbdcc5d46eca52fc43ee84e7062614c
jclingan <jclingan@redhat.com>
2019-09-13 14:34:06 -0700

    Updated application.properties for production.

--- quarkus-springmp-person/src/main/java/org/acme/springmp/PersonSpringMPService.java
+++ quarkus-springmp-person/src/main/java/org/acme/springmp/PersonSpringMPService.java
@@ -1,31 +1,32 @@
 package org.acme.springmp;
 
-import javax.inject.Inject;
-
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 public class PersonSpringMPService {
 
-    @Inject
+    @Autowired
     @RestClient
     SalutationMicroProfileRestClient salutationRestClient;
 
     @Value("${fallbackSalutation}")
     String fallbackSalutation;
 
-    @Timeout(value = 500 )
+    @Timeout(500)
     @Fallback(fallbackMethod = "salutationFallback")
     public String getSalutation() {
         String salutation = salutationRestClient.getSalutation();
 
         return salutation;
+
     }
 
-    @Counted(name = "salutationFallback")
+    //@Metric // Commented out to simplify metrics output
+    @Counted(name = "fallbackCounter", displayName = "Fallback Counter", description = "Salutation Fallback Counter")
     public String salutationFallback() {
         return fallbackSalutation;
     }
--- quarkus-springmp-person/src/test/java/org/acme/springmp/NativePersonControllerIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.springmp;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativePersonControllerIT extends PersonControllerTest {
-
-    // Execute the same tests but in native mode.
-}
