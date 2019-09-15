a81e69148bfac8e744beb7d847a2d414f17ac909
jclingan <jclingan@redhat.com>
2019-09-15 13:51:57 -0700

    Updated to 0.22.0

--- quarkus-springmp-person/src/main/java/org/acme/springmp/PersonSpringMPService.java
+++ quarkus-springmp-person/src/main/java/org/acme/springmp/PersonSpringMPService.java
@@ -6,7 +6,9 @@ import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonSpringMPService {
 
     @Autowired
