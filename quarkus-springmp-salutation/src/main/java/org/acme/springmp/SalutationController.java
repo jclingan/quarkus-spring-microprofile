175913db11d5739d590c8610831d031599a06b49
jclingan <jclingan@redhat.com>
2019-10-30 13:58:41 -0700

    Updated to Quarkus 0.27.0. Moved @RequestMapping to method.

--- quarkus-springmp-salutation/src/main/java/org/acme/springmp/SalutationController.java
+++ quarkus-springmp-salutation/src/main/java/org/acme/springmp/SalutationController.java
@@ -4,17 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping("/salutation")
 public class SalutationController {
 
     SalutationService salutationService;
+
     public SalutationController(SalutationService salutationService) {
         this.salutationService = salutationService;
     }
 
     @GetMapping
+    @RequestMapping("/salutation")
     public String hello() {
         return salutationService.getSalutation();
     }
